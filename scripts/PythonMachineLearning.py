918c0ad3a4071b14a3d92db737eed93d04edd5da
Shaw <ronnshaw@9801a79102c3.ant.amazon.com>
2017-03-31 22:25:50 -0700

    Progress from 3312017

--- scripts/PythonMachineLearning.py
+++ scripts/PythonMachineLearning.py
@@ -544,5 +544,14 @@ plt.show()
 
 # In[ ]:
 
-
+from sklearn.svm import SVC
+svm = SVC(kernel='linear', C=1.0, random_state=0)
+svm.fit(X_train_std, y_train)
+plot_decision_regions(X_combined_std, 
+                      y_combined, classifier=svm,
+                      test_idx=range(105,150))
+plt.xlabel('petal length [standardized]')
+plt.ylabel('petal width [standardized]')
+plt.legend(loc='upper left')
+plt.show()
 
